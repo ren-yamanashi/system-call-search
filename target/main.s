aaf91a48b4f3f6edcd9083ab99f322a94e64516a
ren_yamanashi <ren_yamanashi@mates-system.com>
2023-08-27 14:46:05 +0900

    serch write system call .step2

--- target/main.s
+++ target/main.s
@@ -5,12 +5,34 @@
 _main:                                  ; @main
 	.cfi_startproc
 ; %bb.0:
-	sub	sp, sp, #16
-	.cfi_def_cfa_offset 16
-	str	wzr, [sp, #12]
+	sub	sp, sp, #48
+	stp	x29, x30, [sp, #32]             ; 16-byte Folded Spill
+	add	x29, sp, #32
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	mov	w8, #0
+	str	w8, [sp, #12]                   ; 4-byte Folded Spill
+	stur	wzr, [x29, #-4]
+	adrp	x8, l_.str@PAGE
+	add	x8, x8, l_.str@PAGEOFF
+	str	x8, [sp, #16]
+	ldr	x8, [sp, #16]
+	str	x8, [sp]                        ; 8-byte Folded Spill
+	ldr	x0, [sp, #16]
+	bl	_strlen
+	ldr	x1, [sp]                        ; 8-byte Folded Reload
+	mov	x2, x0
 	mov	w0, #1
-	add	sp, sp, #16
+	bl	_write
+	ldr	w0, [sp, #12]                   ; 4-byte Folded Reload
+	ldp	x29, x30, [sp, #32]             ; 16-byte Folded Reload
+	add	sp, sp, #48
 	ret
 	.cfi_endproc
                                         ; -- End function
+	.section	__TEXT,__cstring,cstring_literals
+l_.str:                                 ; @.str
+	.asciz	"Hello, world!\n"
+
 .subsections_via_symbols
